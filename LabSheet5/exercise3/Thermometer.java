28ea9862a5b4f3b614b5ca80dec1cb2da4982203
Hyden Johnson <hydenjohnson@yahoo.co.uk>
2021-10-08 10:57:37 +0100

    Worked on LabSheets, got onto 5. 08/10/2021

---
+++ LabSheet5/exercise3/Thermometer.java
@@ -0,0 +1,74 @@
+package LabSheet5.exercise3;
+
+public class Thermometer {
+
+    private int currentTemp;
+    private int maximumTemp;
+    private int minimumTemp;
+
+
+    public Thermometer(){
+        this(0);
+        this.minimumTemp = Integer.MAX_VALUE;
+        this.maximumTemp = Integer.MIN_VALUE;
+    }
+
+    public Thermometer(int currentTemp){
+        this.setCurrentTemp(currentTemp);
+        this.setMaximumTemp(currentTemp);
+        this.setMinimumTemp(currentTemp);
+    }
+
+    public void setCurrentTemp(int currentTemp) {
+        this.currentTemp = currentTemp;
+    }
+
+    public void setMaximumTemp(int maximumTemp) {
+        this.maximumTemp = maximumTemp;
+    }
+
+    public void setMinimumTemp(int minimumTemp) {
+        this.minimumTemp = minimumTemp;
+    }
+
+    public int getCurrentTemp() {
+        return currentTemp;
+    }
+
+    public int getMaximumTemp() {
+        return maximumTemp;
+    }
+
+    public int getMinimumTemp() {
+        return minimumTemp;
+    }
+
+
+
+    public  void setTemperature(int userTemp){
+
+        this.currentTemp = userTemp;
+        //this.maximumTemp = userTemp;
+        //this.minimumTemp = userTemp;
+
+        this.updateMaxMinTemp();
+
+    }
+
+    private void updateMaxMinTemp(){
+
+        if(currentTemp >= maximumTemp){
+            setMaximumTemp(currentTemp);
+        }
+
+        if(currentTemp <= minimumTemp){
+            setMinimumTemp(currentTemp);
+        }
+
+    }
+
+    public String toString(){
+        return "Current temperature : " + getCurrentTemp() + "\nMaximum Temperature: " + getMaximumTemp() + "\nMinimum Temperature : " + getMinimumTemp();
+    }
+
+}
