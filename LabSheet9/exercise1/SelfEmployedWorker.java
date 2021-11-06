bb3a26d137841bc54754cf4cd625c1ab6a377faf
ToxicOddesey <78604065+ToxicOddesey@users.noreply.github.com>
2021-11-06 13:29:11 +0000

    06/11/21

--- LabSheet9/exercise1/SelfEmployedWorker.java
+++ LabSheet9/exercise1/SelfEmployedWorker.java
@@ -1,4 +1,28 @@
 package LabSheet9.exercise1;
 
-public class SelfEmployedWorker{
+public final class SelfEmployedWorker extends Employee{
+
+    private double estimatedIncome;
+
+
+    public SelfEmployedWorker(String firstName, String lastName) {
+        super(firstName, lastName);
+        setEstimatedIncome();
+    }
+
+    public void setEstimatedIncome(){
+
+       this.estimatedIncome =(int)(Math.random() * ((900 - 400+1) + 400));
+
+    }
+
+    public double earnings(){
+
+        return estimatedIncome;
+    }
+
+    @Override
+    public String toString() {
+        return "Self employed worker's name is " + super.toString();
+    }
 }
